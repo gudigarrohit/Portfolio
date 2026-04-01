434474a790924ecaa51f0475a419e2b6b38ae7c2
gudigarrohit <gudiagrrohit@gmail.com>
2026-04-01 23:30:13 +0530

    push

--- src/component/sections/Gallery.js
+++ src/component/sections/Gallery.js
@@ -37,7 +37,7 @@ export default function Gallery({ refresh, setIsModalOpen }) {
   return (
     <>
       {/* 🔳 GRID */}
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-4 p-4 max-w-7xl mx-auto">
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-4 p-4 max-w-7xl mx-auto ">
         {images.map((img) => (
           <div
             key={img._id}
